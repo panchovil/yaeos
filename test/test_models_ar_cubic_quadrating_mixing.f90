c5762124a2664517dbfca5ce70c3117d39c12752
Federico E. Benelli <fedebenelli@outlook.com>
2024-03-11 13:17:05 -0300

    format

--- test/test_models_ar_cubic_quadrating_mixing.f90
+++ test/test_models_ar_cubic_quadrating_mixing.f90
@@ -31,16 +31,16 @@ contains
         z = [0.3, 0.5, 0.2]
 
         call mixrule%D1mix(z, del1, D1, dD1i, dD1ij)
-         d1_val = 0.43000000342726713     
+        d1_val = 0.43000000342726713
         dD1i_val = [-0.22999999701976787, 6.9999995529651651E-002,  0.17000001788139310]
         dD1ij_val = reshape([0.45999998718500179, 0.15999999910593043, 5.9999978244305405E-002, &
-                             0.15999999910593043, -0.13999998897314089, -0.24000000983476594, &
-                             5.9999978244305405E-002, -0.24000000983476594,      -0.34000003069639095], [n,n])
+            0.15999999910593043, -0.13999998897314089, -0.24000000983476594, &
+            5.9999978244305405E-002, -0.24000000983476594,      -0.34000003069639095], [n,n])
 
         call check(error, allclose([D1], [D1_val], absolute_tolerance))
         call check(error, allclose([dD1i], [dD1i_val], absolute_tolerance))
         call check(error, allclose([dD1ij], [dD1ij_val], absolute_tolerance))
-        
+
 
     end subroutine test_QMR_RKPR
 
