9515d2f735a12c31f44b3de94ddaa73bfa1ca577
panchovil <matias.rafael.ferreyra@gmail.com>
2025-01-16 03:52:05 -0300

    pruebas con fluidos

--- app/sandoval_phase_test.f90
+++ app/sandoval_phase_test.f90
@@ -5,7 +5,7 @@ program sandoval_phase_test
       SoaveRedlichKwong, PengRobinson76, PengRobinson78, RKPR, PengRobinson78Nano,&
       EquilibriumState, ArModel, PTEnvel2, &
       pt_envelope_2ph, saturation_pressure, saturation_temperature, k_wilson 
-
+   use hyperdual_pr78_nano
    implicit none
 
    ! ===========================================================================
@@ -14,85 +14,144 @@ program sandoval_phase_test
    integer, parameter :: nc=7  
    integer :: i
    class(ArModel), allocatable :: model, model_nano ! Thermodynamic model to be used
-   type(EquilibriumState) :: sat_point, sat_point_nano              ! Init
-   type(PTEnvel2) :: envelope, envelope_nano                       ! PT Phase envelope
+   type(EquilibriumState) :: sat_point, sat_point_nano, sat_point_auto_eos              ! Init
+   type(PTEnvel2) :: envelope, envelope_nano, envelope_auto_eos  
+   class(PR78_nano_autodiff), allocatable :: auto_eos 
+                       ! PT Phase envelope
    real(pr) :: tc(nc), pc(nc), w(nc)                ! Component's critical constants
    real(pr) :: z(nc), kij(nc,nc), lij(nc,nc)        ! Termodynamic variables
    real(pr) :: rp, LJ_par(nc)                       ! Nano parameters
    real(pr) :: pcc(nc), delP(nc), tcc(nc), delT(nc)
+
+   character(len=500) :: header, names(nc)  ! Cadena para almacenar el encabezado
+   character(len=1000) :: fmt
+   
    ! ===========================================================================
    ! Compound definition
    ! ---------------------------------------------------------------------------
    !! names="N2" "CH4" "C2H6" "C3H8" "nC4" "nC5" "nC6"
+   
    !! composition vector                        
-   z = (/0.014, 0.943, 0.027, 0.0074, 0.0049, 0.0027, 0.001/)
-   !! Critical Pressure
-   pc = (/34.0, 45.99, 48.72, 42.48, 37.96, 33.70, 30.25/)
-   !! Critical Temperature
-   tc = (/126.20, 190.56, 305.32, 369.83, 425.12, 469.70, 507.60/)
-   !! Acentric Factor
-   w = (/0.0377, 0.0115, 0.0995, 0.1523, 0.2002, 0.2515, 0.3013/)
-   lij = 0
-   kij = 0
-   Kij(1,2) = 0.0278
-   Kij(1,3) = 0.4070
-   Kij(1,4) = 0.0763
-   Kij(1,5) = 0.0700
-   Kij(1,6) = 0.0787
-   Kij(1,7) = 0.1496
+   z = (/0.0014_pr, 0.943_pr, 0.027_pr, 0.0074_pr, 0.0049_pr, 0.0027_pr, 0.001_pr/)
+   !z = (/0.0014_pr, 0.943_pr, 0.027_pr, 0.0074_pr, 0.0049_pr/)
 
-   Kij(2,1) = 0.0278
-   Kij(2,3) = -0.0078
-   Kij(2,4) = 0.0090
-   Kij(2,5) = 0.0056
-   Kij(2,6) = 0.0190
-   Kij(2,7) = 0.0374
+   z = z/sum(z)
+   print*, z
+   print*, sum(z)
+   !! Critical Pressure
+   pc = (/34.0_pr, 45.99_pr, 48.72_pr, 42.48_pr, 37.96_pr, 33.70_pr, 30.25_pr/)
+   !pc = (/34.0_pr, 45.99_pr, 48.72_pr, 42.48_pr, 37.96_pr/)
+
+   !! Critical Temperature
+   tc = (/126.20_pr, 190.56_pr, 305.32_pr, 369.83_pr, 425.12_pr, 469.70_pr, 507.60_pr/)
+   !tc = (/126.20_pr, 190.56_pr, 305.32_pr, 369.83_pr, 425.12_pr/)
+
+   !! Acentric Factor
+   w = (/0.0377_pr, 0.0115_pr, 0.0995_pr, 0.1523_pr, 0.2002_pr, 0.2515_pr, 0.3013_pr/)
+   !w = (/0.0377_pr, 0.0115_pr, 0.0995_pr, 0.1523_pr, 0.2002_pr/)
+
+   lij = 0.0_pr
+   kij = 0.0_pr
+   Kij(1,2) = 0.0278_pr
+   Kij(1,3) = 0.4070_pr
+   Kij(1,4) = 0.0763_pr
+   Kij(1,5) = 0.0700_pr
+   Kij(1,6) = 0.0787_pr
+   Kij(1,7) = 0.1496_pr
+
+   Kij(2,1) = 0.0278_pr
+   Kij(2,3) = -0.0078_pr
+   Kij(2,4) = 0.0090_pr
+   Kij(2,5) = 0.0056_pr
+   Kij(2,6) = 0.0190_pr
+   Kij(2,7) = 0.0374_pr
    
-   rp = 150 !nm
+   rp = 50.0_pr !nm
    LJ_par = (/0.364_pr, 0.375_pr, 0.443_pr, 0.472_pr, 0.506_pr, 0.529_pr, 0.550_pr/) 
-   
-   !! Critical Pressure shift
-   delP = 0.9793*((rp/LJ_par)**-0.6366)
-   !! Critical Pressure modified
-   pcc = -((delP*pc)-pc)
-   !! Critical Temperature shift
-   delT = 0.7597*((rp/LJ_par)**-0.7708)
-   !! Critical Pressure modified
-   tcc = -((delT*tc)-tc)
+!   LJ_par = (/0.364_pr, 0.375_pr, 0.443_pr, 0.472_pr, 0.506_pr/) 
+
+   ! !! Critical Pressure shift
+   ! delP = 0.9793*((rp/LJ_par)**-0.6366)
+   ! !! Critical Pressure modified
+   ! pcc = -((delP*pc)-pc)
+   ! !! Critical Temperature shift
+   ! delT = 0.7597*((rp/LJ_par)**-0.7708)
+   ! !! Critical Pressure modified
+   ! tcc = -((delT*tc)-tc)
 
    ! Model definition
    model = PengRobinson78(tc, pc, w, kij, lij)
    model_nano = PengRobinson78Nano(LJ_par, rp, tc, pc, w, kij, lij)
+   auto_eos = setup(LJ_par=LJ_par, rp=rp, tc=tc, pc=pc, w=w, kij=kij, lij=lij)
    
    ! Calculate a dew point at low pressure to later 
    ! initialize the phase envelope
    ! sat_point = saturation_temperature(model, z, P=1._pr, kind="dew", t0=300._pr)
    ! sat_point_nano = saturation_temperature(model_nano, z, P=1._pr, kind="dew", t0=300._pr)
-   print*, "pc", pc
-   print*, "pcc", pcc
-   print*, "tc", tc
-   print*, "tcc", tcc
+   ! print*, "pc", pc
+   ! print*, "pcc", pcc
+   ! print*, "tc", tc
+   ! print*, "tcc", tcc
 
+
+   ! Inicia el encabezado con "T, P"
+   names(1) = "N2"
+   names(2) = "CH4"
+   names(3) = "C2H6"
+   names(4) = "C3H8"
+   names(5) = "nC4"
+   names(6) = "nC5"
+   names(7) = "nC6"
+
+   !names=(/"N2", "CH4", "C2H6", "C3H8", "nC4", "nC5", "nC6"/)
+   header = "         T                     P"
+
+   ! Añade "lnK1", "lnK2", ..., "lnKn" dinámicamente
+   do i = 1, nc
+      header = trim(adjustl(header)) // "          lnK" // trim(adjustl(itoa(i))) // "(" // trim(adjustl(names(i))) // ")"
+
+      !write(header(len_trim(header)+1:), "(A, '          lnK', I0)") " ", i,"(",names(i),")"
+   end do
+
+   ! Define el formato fijo para los datos
+   fmt = "(F25.16, F25.16"  ! Espacio fijo para "T" y "P"
+   do i = 1, nc
+       fmt = trim(fmt) // ", F25.16"  ! Espacio fijo para cada "lnK"
+   end do
+   fmt = trim(fmt) // ")"
    ! ------------------------- BUBBLE ENVELOPE ------------------
 
    sat_point = saturation_temperature(model, z, P=1.5_pr, kind="bubble", t0=115._pr)
    sat_point_nano = saturation_temperature(model_nano, z, P=1.5_pr, kind="bubble", t0=115._pr)
+   sat_point_auto_eos = saturation_temperature(auto_eos, z, P=1.5_pr, kind="bubble", t0=115._pr)
    ! Calculate phase envelope
    envelope = pt_envelope_2ph(model, z, sat_point)
    envelope_nano = pt_envelope_2ph(model_nano, z, sat_point_nano, points=500, iterations=1000)
+   envelope_auto_eos = pt_envelope_2ph(auto_eos, z, sat_point_auto_eos)
 
    !write(1, "(*(A,2x))") "kind","T", "P", "beta","x", "y", "Vx", "Vy"
    !write(*, *) envelope%points(1)
-   do i=1,size(envelope%points)
-      write(1,*) envelope%points(i)%T ,envelope%points(i)%P, log(envelope%points(i)%y/envelope%points(i)%x)
-   end do
-   write(1,*), envelope%cps
+   write(1, "(A)") trim(header)
+   ! do i=1,size(envelope%points)
+   !    write(1,fmt) envelope%points(i)%T ,envelope%points(i)%P,log(envelope%points(i)%y/envelope%points(i)%x)
+   !    !write(1,*) log(envelope%points(i)%y/envelope%points(i)%x)
+   ! end do
+   write(1,*) envelope
+   ! write(1,*), envelope%cps
    !write(2, "(*(A,2x))") "kind","T", "P", "beta","x", "y", "Vx", "Vy"
    !write(*, *) envelope_nano%points(28)
-   do i=1,size(envelope_nano%points)
-      write(3,*) envelope_nano%points(i)%T ,envelope_nano%points(i)%P
-   end do
-   
+   write(3, "(A)") trim(header)
+   ! do i=1,size(envelope_nano%points)
+   !    !write(1,*) envelope_nano%points(i)%T ,envelope_nano%points(i)%P
+   !    write(3,fmt) envelope_nano%points(i)%T ,envelope_nano%points(i)%P, log(envelope_nano%points(i)%y/envelope_nano%points(i)%x)
+   ! end do
+   write(3,*) envelope_nano
+   ! do i=1,size(envelope_auto_eos%points)
+   !    !write(3,*) envelope_auto_eos%points(i)%T ,envelope_auto_eos%points(i)%P
+   !    write(3,*) envelope_auto_eos%points(i)%T ,envelope_auto_eos%points(i)%P, &
+   !    log(envelope_auto_eos%points(i)%y/envelope_auto_eos%points(i)%x)
+   ! end do
+   !write(3,*) envelope_auto_eos
    !write(2,*) envelope_nano
    !print*, size(envelope_nano%points)
 
@@ -100,9 +159,11 @@ program sandoval_phase_test
 
    sat_point = saturation_temperature(model, z, P=0.5_pr, kind="dew", t0=200._pr)
    sat_point_nano = saturation_temperature(model_nano, z, P=0.5_pr, kind="dew", t0=200._pr)
+   sat_point_auto_eos = saturation_temperature(auto_eos, z, P=0.5_pr, kind="dew", t0=200._pr)
 
    envelope = pt_envelope_2ph(model, z, sat_point)
    envelope_nano = pt_envelope_2ph(model_nano, z, sat_point_nano, points=500, iterations=500)
+   envelope_auto_eos = pt_envelope_2ph(auto_eos, z, sat_point)
 
    ! write(3, "(*(A,2x))") "kind","T", "P", "beta","x", "y", "Vx", "Vy"
    ! write(*, *) envelope%points(1)
@@ -111,14 +172,32 @@ program sandoval_phase_test
    ! write(4, "(*(A,2x))") "kind","T", "P", "beta","x", "y", "Vx", "Vy"
    ! write(*, *) envelope_nano%points(1)
    ! write(4,*) envelope_nano
+   write(2, "(A)") trim(header)
 
    do i=1,size(envelope%points)
-      write(2,*) envelope%points(i)%T ,envelope%points(i)%P
-   end do
-
-   do i=1,size(envelope_nano%points)
-      write(4,*) envelope_nano%points(i)%T ,envelope_nano%points(i)%P
+      !write(2,*) envelope%points(i)%T ,envelope%points(i)%P
+      write(2,fmt) envelope%points(i)%T ,envelope%points(i)%P, log(envelope%points(i)%y/envelope%points(i)%x)
    end do
    
+   write(4, "(A)") trim(header)
+   do i=1,size(envelope_nano%points)
+      !write(2,*) envelope_nano%points(i)%T ,envelope_nano%points(i)%P
+      write(4,fmt) envelope_nano%points(i)%T ,envelope_nano%points(i)%P, log(envelope_nano%points(i)%y/envelope_nano%points(i)%x)
+   end do
+   ! do i=1,size(envelope_auto_eos%points)
+   !    !write(4,*) envelope_auto_eos%points(i)%T ,envelope_auto_eos%points(i)%P
+   !    write(4,*) envelope_auto_eos%points(i)%T ,envelope_auto_eos%points(i)%P, &
+   !    log(envelope_auto_eos%points(i)%y/envelope_auto_eos%points(i)%x)
+   ! end do
+   ! !write(4,*) envelope_auto_eos
+   ! write(6,*) "asd"
+
+   contains
+
+   ! Función para convertir números enteros a cadenas (para el índice i)
+   character(len=10) function itoa(i)
+     integer, intent(in) :: i
+     write(itoa, '(I0)') i
+   end function itoa
 
 end program sandoval_phase_test
--- app/test_nano_autodiff.f90
+++ app/test_nano_autodiff.f90
@@ -162,40 +162,145 @@ contains
 end module hyperdual_pr78_nano
 
 
-program test_nano_autodiff
-   use hyperdual_pr78_nano
-   implicit none
-   class(PR78_nano_autodiff), allocatable :: auto_eos
+! program test_nano_autodiff
+!    use hyperdual_pr78_nano
+!    use yaeos, only: pr, R, Substances, AlphaSoave, CubicEoSNano, CubicEoS, &
+!    GenericCubic_Ar_Nano, GenericCubic_Ar
+!    use yaeos, only: ArModel, PengRobinson78Nano
+!    implicit none
+!    class(PR78_nano_autodiff), allocatable :: auto_eos
+!    class(CubicEoSNano), allocatable :: eos
    
-   integer, parameter :: n=4
-   ! real(pr) :: z(n)
-   ! real(pr) :: v=1.0, t=150.0, p
-   type(hyperdual) :: v, t, p , z(n), ar_result
+!    integer, parameter :: n=4
+!    real(pr) :: z(n)
+!    real(pr) :: v, t, p
+!    ! type(hyperdual) :: v, t, p , z(n), ar_result
+!    real(pr) :: ar
+!    real(pr) :: art, arv, arv2, art2, artv
+!    real(pr) :: arn(size(z)), arvn(size(z)), artn(size(z)), arn2(size(z),size(z)) 
+!    real(pr) :: auto_ar
+!    real(pr) :: auto_art, auto_arv, auto_arv2, auto_art2, auto_artv
+!    real(pr) :: auto_arn(size(z)), auto_arvn(size(z)), auto_artn(size(z)), auto_arn2(size(z),size(z)) 
+
+
+!    real(pr) :: tc(size(z)), pc(size(z)), w(size(z)), kij(size(z), size(z)), lij(size(z), size(z)), LJ_par(size(z)), rp
+!    integer :: i
+
+
+! !CH4, CO2, C4H10(Butano), C6H14(Hexano)
+!    tc = [190.56_pr, 304.13_pr, 452.2_pr, 507.9_pr]
+!    pc = [45.99_pr, 73.8_pr, 37.96_pr, 30.25_pr]
+!    w = [0.011_pr, 0.225_pr, 0.199_pr, 0.297_pr]
+!    LJ_par = [0.3758_pr, 0.300_pr, 0.443_pr, 0.481_pr] !nm 
+!    rp = 100.0_pr !nm
+
+!    kij = reshape([0.000_pr, 0.110_pr, 0.025_pr, 0.030_pr, &
+!                   0.110_pr, 0.000_pr, 0.120_pr, 0.140_pr, &
+!                   0.025_pr, 0.120_pr, 0.000_pr, 0.015_pr, &
+!                   0.030_pr, 0.140_pr, 0.015_pr, 0.000_pr], [n,n]) 
+!    lij = kij / 2 
+
+!    auto_eos = setup(LJ_par=LJ_par, rp=rp, tc=tc, pc=pc, w=w, kij=kij, lij=lij)
+!    eos = PengRobinson78Nano(LJ_par=LJ_par, rp=rp, tc=tc, pc=pc, w=w, kij=kij, lij=lij)
+
+
+!    z = [0.7_pr, 0.1_pr, 0.1_pr, 0.1_pr] 
+!    t = 250._pr
+!    v = 100.0_pr
+!    !ar_result = auto_eos%Ar(z, v, t)
+!    !arfun(auto_eos, z, v, t)
+!    !print*, ar_result
+!    Ar=0
+!    ArV=0
+!    arv2=0
+!    Art=0
+!    art2=0
+!    artv=0
+!    arn=0
+!    artn=0
+!    arvn=0
+!    arn2=0
    
-   real(pr) :: tc(size(z)), pc(size(z)), w(size(z)), kij(size(z), size(z)), lij(size(z), size(z)), LJ_par(size(z)), rp
+!    auto_ar=0
+!    auto_arv=0
+!    auto_arv2=0
+!    auto_art=0
+!    auto_art2=0
+!    auto_artv=0
+!    auto_arn=0
+!    auto_artn=0
+!    auto_arvn=0
+!    auto_arn2=0
+
+!    call auto_eos%residual_helmholtz(n=z, v=v, t=t, Ar=auto_ar, ArV=auto_arv, ArT=auto_art,&
+!    ArTV=auto_artv, ArV2=auto_arv2, art2=auto_art2, Arn=auto_Arn, ArVn=auto_arvn, &
+!    ArTn=auto_artn, Arn2=auto_arn2)
+!    call eos%residual_helmholtz(&
+!    n=z, v=v, t=t, Ar=Ar, ArV=ArV, ArV2=ArV2, ArT=ArT, ArTV=ArTV, &
+!    ArT2=ArT2, Arn=Arn, ArVn=ArVn, ArTn=ArTn, Arn2=Arn2 &
+!    )
 
 
 
-!CH4, CO2, C4H10(Butano), C6H14(Hexano)
-   tc = [190.56_pr, 304.13_pr, 452.2_pr, 507.9_pr]
-   pc = [45.99_pr, 73.8_pr, 37.96_pr, 30.25_pr]
-   w = [0.011_pr, 0.225_pr, 0.199_pr, 0.297_pr]
-   LJ_par = [0.3758_pr, 0.300_pr, 0.443_pr, 0.481_pr] !nm 
-   rp = 100.0_pr !nm
+!    print *, "Ar: ", ar
+!    print *, "auto_Ar: ", auto_ar
+!    print *, "Ar diff", abs(ar-auto_ar)
+!    print *, "-----------------------------------------------------------"
 
-   kij = reshape([0.000_pr, 0.110_pr, 0.025_pr, 0.030_pr, &
-                  0.110_pr, 0.000_pr, 0.120_pr, 0.140_pr, &
-                  0.025_pr, 0.120_pr, 0.000_pr, 0.015_pr, &
-                  0.030_pr, 0.140_pr, 0.015_pr, 0.000_pr], [n,n]) 
-   lij = kij / 2 
 
-   auto_eos = setup(LJ_par=LJ_par, rp=rp, tc=tc, pc=pc, w=w, kij=kij, lij=lij)
+!    print *, "ArV: ", arV
+!    print *, "auto_ArV: ", auto_arv
+!    print *, "ArV diff", abs(arv-auto_arv)
+!    print *, "-----------------------------------------------------------"
+
+!    print *, "ArT: ", arT
+!    print *, "auto_ArT: ", auto_art
+!    print *, "ArT diff", abs(art-auto_art)
+!    print *, "-----------------------------------------------------------"
+
+!    print *, "ArT2: ", arT2
+!    print *, "auto_ArT2: ", auto_art2
+!    print *, "ArT2 diff", abs(art2-auto_art2)
+!    print *, "-----------------------------------------------------------"
+
+!    print *, "ArV2: ", ArV2
+!    print *, "auto_ArV2: ", auto_arv2
+!    print *, "ArV2 diff", abs(arv2-auto_arv2)
+!    print *, "-----------------------------------------------------------"
    
-   z = [0.1_pr, 0.4_pr, 0.3_pr, 0.2_pr] 
-   t = 150._pr
-   v = 1.0_pr
-   ar_result = auto_eos%Ar(z, v, t)
-   !arfun(auto_eos, z, v, t)
+!    print *, "ArTV: ", ArTV
+!    print *, "auto_ArTV: ", auto_arTv
+!    print *, "ArTV diff", abs(artv-auto_artv)
+!    print *, "-----------------------------------------------------------"
+   
+!    print *, "Arn: ", Arn
+!    print *, "auto_Arn: ", auto_arn
+!    print *, "Arn diff", abs(arn-auto_arn)
+!    print *, "-----------------------------------------------------------"
 
-   print*, ar_result
-end program test_nano_autodiff
+!    print *, "ArVn: ", ArVn
+!    print *, "auto_ArVn: ", auto_arvn
+!    print *, "ArVn diff", abs(arvn-auto_arvn)
+!    print *, "-----------------------------------------------------------"
+
+!    print *, "ArTn: ", ArTn
+!    print *, "auto_ArTn: ", auto_arTn
+!    print *, "ArTn diff", abs(artn-auto_artn)
+!    print *, "-----------------------------------------------------------"
+
+!    print *, "Arn2: "
+!    do i=1,n
+!       print*, Arn2(i,:)
+!    end do
+!    print *, "auto_Arn2: "
+!    do i=1,n
+!       print*, auto_arn2(i,:)
+!    end do
+!    print *, "Arn2 diff "
+!    do i=1,n
+!       print*, abs(Arn2(i,:)-auto_arn2(i,:))
+!    end do
+   
+   
+
+! end program test_nano_autodiff
